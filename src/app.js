6461d7cb27f372e1ec58fc3ee8013214f3c89a28
Martin <martin@chispalab.co>
2023-10-21 10:22:21 -0300

    modified cookie settings

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.use(
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
-    cookie: {},
+    cookie: { sameSite: "none", httpOnly: true },
   }),
 );
 
