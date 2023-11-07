f1ede01927d8d297e334c099645726da285bc8a3
Martin <martin@chispalab.co>
2023-11-07 18:44:59 -0300

    changed cookie name

--- src/routes/users.router.js
+++ src/routes/users.router.js
@@ -24,7 +24,7 @@ router.post("/login", async (req, res) => {
       const token = userService.generateToken(user);
       res
         .status(200)
-        .cookie(process.env.TOKEN_COOKIE_NAME, token)
+        .cookie(process.env.TOKEN_COOKIE_NAME, token, { path: "/", domain: ".ringm.com.ar" })
         .json({ status: "success", message: "Log in successfull" });
     } else {
       res.status(401).json({ status: "Unauthorized", message: "Invalid credentials." });
